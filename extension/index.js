f177bd2b39c0a23ff82d4c286646b8a64562daf3
Rodrigo-Mirage <mirage4d@hotmail.com>
2022-05-31 17:49:55 -0300

    rename

--- extension/index.js
+++ extension/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const {OBSUtility} = require('nodecg-utility-obs');
+//const {OBSUtility} = require('nodecg-utility-obs');
 const timerObj = require('./timer');
 const fileSystem = require('./fileSystem');
 const Videos = require('./videoDown');
 
 module.exports = function (nodecg) {
-    const obs = new OBSUtility(nodecg);
+    //const obs = new OBSUtility(nodecg);
     const tim = new timerObj(nodecg);
     const files = new fileSystem(nodecg);
     const videos = new Videos(nodecg);
