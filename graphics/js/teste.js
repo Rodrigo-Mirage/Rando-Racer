b6f21fb72d1dca573ad0c2931e6e1e84a71650e9
Rodrigo-Mirage <mirage4d@hotmail.com>
2022-06-01 15:54:45 -0300

    teste

--- graphics/js/teste.js
+++ graphics/js/teste.js
@@ -2,6 +2,7 @@ var player = videojs('vid1');
 
 var options ={
     src: "/m3u8/egodev",
-    type: 'application/x-mpegURL'
+    type: 'application/x-mpegURL',
+    withCredencial:true
 };
 player.src(options);
